e5e96edc5a9922c57196fc661ef3d20197195ffd
Novivy <tromp.pierre@gmail.com>
2026-04-23 15:13:48 +0200

    v1.0.7

--- main.cpp
+++ main.cpp
@@ -1600,7 +1600,6 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
             g_pTaskbar->SetProgressState(g_hwnd, ok ? TBPF_NOPROGRESS : TBPF_ERROR);
         if (ok) RefreshVersionLabels();
 
-
         if (ok) std::thread(RunLauncherUpdateCheck).detach();
 
         if (ok && g_freshInstall) {
